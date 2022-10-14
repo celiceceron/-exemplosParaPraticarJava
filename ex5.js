3a8a54c0845464e758684f6bcc32d6eacdb2578a
celiceceron <celice.cer@gmail.com>
2022-10-15 01:39:18 +0200

    soma eretorno de menssagem

--- ex5.js
+++ ex5.js
@@ -1,2 +1,11 @@
 /* Somar dois numeros e retornar: A soma deu: XX
 */
+
+function somaDeDoisNumeros(num1, num2){
+
+    const soma = (num1 + num2);
+    const retornar = "A soma deu " + soma;
+    return retornar
+}
+
+console.log(somaDeDoisNumeros(5,6));
