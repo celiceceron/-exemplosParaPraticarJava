bbe9495ff50f5ad5aeb3f4dc0422f2a01e4d0edc
celiceceron <celice.cer@gmail.com>
2022-10-15 00:40:56 +0200

    concatenar strings

--- ex2.js
+++ ex2.js
@@ -3,8 +3,8 @@
 
 function calculaMedia (num1, num2){
 
-const operacao = (num1 + num2)/2;
-return operacao;
+    const operacao = (num1 + num2)/2;
+    return operacao;
 }
 
 console.log(calculaMedia(10,20));
---
+++ ex4.js
@@ -0,0 +1,11 @@
+/* Concatenar 2 strings.
+*/
+
+function concatenar (texto1, texto2){
+
+    const palavras = (texto1 + texto2);
+    return palavras;
+}
+
+console.log(concatenar("Sou" , "Programadora!"));
+
