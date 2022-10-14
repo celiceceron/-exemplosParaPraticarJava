ab7089227bb6ea5af03e0366961e92111d6144fa
celiceceron <celice.cer@gmail.com>
2022-10-15 00:31:19 +0200

    retorna resto da divisao

---
+++ ex3.js
@@ -0,0 +1,9 @@
+/* Retornar resto da divisao de dois numeros.
+*/
+
+function retornaResto (num1, num2){
+    const operacao = (num1 % num2);
+    return operacao;
+}
+
+console.log(retornaResto(10,3));
