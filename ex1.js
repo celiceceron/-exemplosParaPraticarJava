9c4c05c8abf21767f0a0f39a7b081fd85f86c585
celiceceron <celice.cer@gmail.com>
2022-10-14 23:49:01 +0200

    soma de parametros

---
+++ ex1.js
@@ -0,0 +1,13 @@
+/* Funcao que retorna a soma de 3 parametros.
+*/
+
+function somaTodos(num1, num2, num3){
+
+    num1 = 20;
+    num2 = 30;
+    num3 = 40;
+
+    const soma = (num1 + num2 + num3);
+    return soma
+}
+
