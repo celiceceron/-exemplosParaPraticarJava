46c6fb324aba0932296a6a4327c05f22291da527
celiceceron <celice.cer@gmail.com>
2022-10-15 02:21:15 +0200

    arrow function maior de idade

--- ex6.js
+++ ex6.js
@@ -1,3 +1,13 @@
 /* Retornar Maior de Idade caso a idade passada seja maior
 que 17 ou Menor de Idade caso contrário.
 */
+
+const maiorDeIdade = (idade) => {
+    if (idade >= 17) {
+        console.log("Maior de idade");
+    } else {
+        console.log("Menor de idade");
+    }
+}
+console.log(maiorDeIdade(17));
+
