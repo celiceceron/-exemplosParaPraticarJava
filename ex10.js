f419284d9317e150245f59cec0120802cbaacd35
celiceceron <celice.cer@gmail.com>
2022-10-15 21:22:38 +0200

    arrow function usando if ou else

--- ex10.js
+++ ex10.js
@@ -1,4 +1,13 @@
 /* Funcao recebe uma nota e um numero de faltas e retorna
 Aprovado caso a media seja maior ou igual a 7 e o numero
-de faltass menor que 10, ou Reprovado caso contrario.
+de faltas menor que 10, ou Reprovado caso contrario.
 */
+
+const consultaDoAluno = (mediaDaNota, faltas) => {
+    if (mediaDaNota >= 7, faltas <10){
+        console.log("Aprovado");
+    } else{
+        console.log("Reprovado");
+    }
+}
+console.log(consultaDoAluno(8,10));
