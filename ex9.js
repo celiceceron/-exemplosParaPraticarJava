93d97a3d4993918d0d25852caf2c6b1d2319aabd
celiceceron <celice.cer@gmail.com>
2022-10-15 22:36:30 +0200

    arrow function usando if ou else e boleano

--- ex9.js
+++ ex9.js
@@ -3,3 +3,13 @@ com desconto ou nao. Se o preco for maior que 100 e nao
 estiver com desconto, a funcao deve retornar Quero desconto.
 Caso contrario, deve retornar Negocio Fechado.
 */
+
+const pecoDesconto = (preco, estaComDesconto) => {
+    if (preco > 100 && estaComDesconto === false){
+    console.log("Quero desconto"); 
+    } else {
+        console.log("Negócio fechado!");
+    }
+}
+
+console.log(pecoDesconto(120));
