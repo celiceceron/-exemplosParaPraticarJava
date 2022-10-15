7a962b838f21cdc243f9a9027eb1ea1ce0348596
celiceceron <celice.cer@gmail.com>
2022-10-15 02:39:08 +0200

    arrow function usando if ou else

--- ex7.js
+++ ex7.js
@@ -2,3 +2,14 @@
  ou igual a 7, Prova Final caso seja maior ou igual a 5, porem,
  menor que 7 ou Reprovado caso contrario.
 */
+
+const informacoesDoAluno = (nota) => {
+    if(nota >= 7){
+        console.log("Aprovado");
+    } else if (nota >=5){
+        console.log("Prova final");
+    } else {
+        console.log("Reprovado");
+    }
+}
+console.log(informacoesDoAluno(9));
