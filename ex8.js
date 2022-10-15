c4c00e57939b271e7d3b11108d25ef73cd0c81df
celiceceron <celice.cer@gmail.com>
2022-10-15 02:49:03 +0200

    arrow function usando if e else

--- ex8.js
+++ ex8.js
@@ -2,3 +2,15 @@
 indicando se a media simples entre as 3 notas, e igual ou
 maior que 7.
 */
+
+const retornoDeAluno = (nota1, nota2, nota3) => {
+    const media = (nota1 + nota2 + nota3)/3;
+
+    if(media >= 7) {
+        console.log("SIM");
+    } else {
+        console.log("NÃO");
+    }
+}
+
+console.log(retornoDeAluno(9,9,7));
